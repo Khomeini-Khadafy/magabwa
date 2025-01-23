ae1e20652f0dda38bd06220b1898e9fa1a84f8ce
Khomeini Khadafy <Khomeini,khadafy@gmail.com>
2025-01-24 00:56:04 +0700

    membuat CRUD Author menambahkan Attribute untuk menyesuaikan slug dan nama

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 
 class Author extends Model
 {
@@ -18,7 +19,19 @@ class Author extends Model
         'slug',
     ];
 
-     /**
+    /**
+     * Set the name attribute and also set the slug attribute based on the value.
+     * Menambahkan Attribut name dan slug untuk Author
+     * @param string $value
+     * @return void
+     */
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+
+    /**
      * Get all of the news for the relation in table ArticleNews, for the author
      * untuk melihat postingan berita yang udah di tulis oleh author tsb
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
