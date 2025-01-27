945ed907734dc14ddff5f33068603e61b054ca3d
Khomeini Khadafy <Khomeini,khadafy@gmail.com>
2025-01-27 17:56:51 +0700

    membuat controller keseluruhan

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [FrontController::class, 'index'])->name('front.index');
+
+Route::get('/details/{article_news:slug}', [FrontController::class, 'details'])->name('front.index');
+
+Route::get('/category/{category:slug}', [FrontController::class, 'category'])->name('front.category');
+
+Route::get('/author/{author:slug}', [FrontController::class, 'author'])->name('front.author');
+
+Route::get('/search/{search:slug}', [FrontController::class, 'search'])->name('front.search');
