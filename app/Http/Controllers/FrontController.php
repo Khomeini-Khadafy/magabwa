127a740e212b2d6aa622bf65e1df44a52e85278b
Khomeini Khadafy <Khomeini,khadafy@gmail.com>
2025-01-28 23:54:47 +0700

    membuat query di bagian controller not_featured dan featured setelah membuat query lanjutkan dengan integrasi dihalaman frontend untuk Entertaiment

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -7,35 +7,61 @@ use App\Models\ArticleNews;
 use App\Models\Author;
 use App\Models\BannerAdvertisement;
 use App\Models\Category;
-use Illuminate\Http\Request;
 
 class FrontController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         $categories = Category::all();
 
         // menampilkan article berdasarkan [not_featured]
         $articles = ArticleNews::with(['category'])
-        ->where('is_featured', 'not_featured')
-        ->latest()
-        ->take(6)
-        ->get();
+            ->where('is_featured', 'not_featured')
+            ->latest()
+            ->take(6)
+            ->get();
 
         // menampilkan article berdasarkan [featured]
         $featured_articles = ArticleNews::with(['category'])
-        ->where('is_featured', 'featured')
-        ->inRandomOrder()
-        ->take(3)
-        ->get();
+            ->where('is_featured', 'featured')
+            ->inRandomOrder()
+            ->take(3)
+            ->get();
 
         $authors = Author::all();
 
+        // untuk bannerads atau iklan
         $bannerads = BannerAdvertisement::where('is_active', 'active')
-        ->where('type', 'banner')
-        ->inRandomOrder()
+            ->where('type', 'banner')
+            ->inRandomOrder()
         // ->take()
-        ->first();
+            ->first();
 
-        return view ('front.index', compact('categories', 'articles', 'authors', 'featured_articles', 'bannerads'));
+        // buat variabel baru lalu ambil modelnya ArticleNews| whereHas(dimana) masuk ke relasi category pada model articleNews
+        $entertaiment_articles = ArticleNews::whereHas('category', function ($query) {
+            $query->where('name', 'Entertaiment');
+        })
+            ->where('is_featured', 'not_featured')
+            ->latest()
+            ->take(6)
+            ->get();
+
+        $entertaiment_articles_featured = ArticleNews::whereHas('category', function ($query) {
+            $query->where('name', 'Entertaiment');
+        })
+            ->where('is_featured', 'featured')
+            ->inRandomOrder()
+            ->first();
+
+        return view('front.index',
+            compact(
+                'entertaiment_articles_featured',
+                'entertaiment_articles',
+                'categories',
+                'articles',
+                'authors',
+                'featured_articles',
+                'bannerads'
+            ));
     }
 }
