c81286300b7fa47f051a4e6d21de3452ea915da5
Khomeini Khadafy <Khomeini,khadafy@gmail.com>
2025-01-30 00:32:59 +0700

    membuat components disetiap halaman nav, dan header

---
+++ resources/views/components/javascript.blade.php
@@ -0,0 +1,8 @@
+<div>
+    <script src="{{ asset('customjs/two-lines-text.js') }}"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <!-- JavaScript -->
+    <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
+    <script src="{{ asset('customjs/carousel.js') }}"></script>
+</div>
