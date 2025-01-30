8d64458f0dc504eac642212e2fdb057347d62952
Khomeini Khadafy <Khomeini,khadafy@gmail.com>
2025-01-31 02:41:40 +0700

    merapihkan code

--- resources/views/front/author.blade.php
+++ resources/views/front/author.blade.php
@@ -7,13 +7,13 @@
     <x-navbar />
 
     <nav id="Category" class="max-w-[1130px] mx-auto flex justify-center items-center gap-4 mt-[30px]">
-        @foreach ($categories as $itemcategory)
-            <a href="{{ route('front.category', $itemcategory->slug) }}"
+        @foreach ($categories as $item_category)
+            <a href="{{ route('front.category', $item_category->slug) }}"
                 class="rounded-full p-[12px_22px] flex gap-[10px] font-semibold transition-all duration-300 border border-[#EEF0F7] hover:ring-2 hover:ring-[#FF6B18]">
                 <div class="w-6 h-6 flex shrink-0">
-                    <img src="{{ Storage::url($itemcategory->icon) }}" alt="icon" />
+                    <img src="{{ Storage::url($item_category->icon) }}" alt="icon" />
                 </div>
-                <span>{{ $itemcategory->name }}</span>
+                <span>{{ $item_category->name }}</span>
             </a>
         @endforeach
     </nav>
@@ -33,31 +33,30 @@
             </div>
         </div>
         <div id="content-cards" class="grid grid-cols-3 gap-[30px]">
-            @forelse ($author->news as $author)
-            <a href="{{ route('front.details', $author->slug) }}" class="card">
-                <div
-                    class="flex flex-col gap-4 p-[26px_20px] transition-all duration-300 ring-1 ring-[#EEF0F7] hover:ring-2 hover:ring-[#FF6B18] rounded-[20px] overflow-hidden bg-white">
-                    <div class="thumbnail-container h-[200px] relative rounded-[20px] overflow-hidden">
-                        <div
-                            class="badge absolute left-5 top-5 bottom-auto right-auto flex p-[8px_18px] bg-white rounded-[50px]">
-                            <p class="text-xs leading-[18px] font-bold">{{ $author->category->name }}</p>
+            @forelse ($author->news as $news)
+                <a href="{{ route('front.details', $news->slug) }}" class="card">
+                    <div
+                        class="flex flex-col gap-4 p-[26px_20px] transition-all duration-300 ring-1 ring-[#EEF0F7] hover:ring-2 hover:ring-[#FF6B18] rounded-[20px] overflow-hidden bg-white">
+                        <div class="thumbnail-container h-[200px] relative rounded-[20px] overflow-hidden">
+                            <div
+                                class="badge absolute left-5 top-5 bottom-auto right-auto flex p-[8px_18px] bg-white rounded-[50px]">
+                                <p class="text-xs leading-[18px] font-bold">{{ $news->category->name }}</p>
+                            </div>
+                            <img src="{{ Storage::url($news->thumbnail) }}" alt="thumbnail photo"
+                                class="w-full h-full object-cover" />
                         </div>
-                        <img src="{{ Storage::url($author->thumbnail) }}" alt="thumbnail photo"
-                            class="w-full h-full object-cover" />
+                        <div class="flex flex-col gap-[6px]">
+                            <h3 class="text-lg leading-[27px] font-bold">
+                                {{ substr($news->name, 0, 80) }}{{ strLen($news->name) > 80 ? '...' : '' }}
+                            </h3>
+                            <p class="text-sm leading-[21px] text-[#A3A6AE]">
+                                {{ $news->created_at->format('M d, Y') }}</p>
+                        </div>
                     </div>
-                    <div class="flex flex-col gap-[6px]">
-                        <h3 class="text-lg leading-[27px] font-bold">
-                            {{ substr($author->name, 0, 80) }}{{ strLen($author->name) > 80 ? '...' : '' }}
-                        </h3>
-                        <p class="text-sm leading-[21px] text-[#A3A6AE]"> 
-                            {{ $author->created_at->format('M d, Y') }}</p>
-                    </div>
-                </div>
-            </a>
+                </a>
             @empty
                 <p>Belum ada berita yang ditulis</p>
             @endforelse
-            
         </div>
     </section>
     {{-- BannerAds --}}
